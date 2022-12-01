0a1df01f07c8947968ac404b1b8ff1af31c2c5ba
Ace <113405563+AceLau139@users.noreply.github.com>
2022-12-01 19:59:23 +0800

    Add files via upload

--- js/cart.js
+++ js/cart.js
@@ -12,10 +12,7 @@ var cartItem = {
 
 window.onload = function() {
 
-    /* var oBox = document.querySelector("#box"); */
     var oCar = document.querySelector("#car");
-
-
     var proImg = localStorage.getItem('imgUrl');
     var productName = localStorage.getItem('productName');
     var productPrice = localStorage.getItem('productPrice');
@@ -24,19 +21,6 @@ window.onload = function() {
     function showCart(){
         proImg.innerHTML += '<div class="img left"><img src="' + productList["imgUrl"] + '" ></div>';
     }
-/* 
-    //add shopping cart btn
-    for (let i = 0; i < product.length; i++) {
-        var oLi = document.createElement("li");
-        var data = product[i];
-
-        oLi.innerHTML += '<div class="pro_img"><img src="' + data["imgUrl"] + '" width="100%" height="100%"></div>';
-        oLi.innerHTML += '<h3 id="h3" class="pro_name"><a rel="nofollow" href="#">' + data["productName"] + '</a></h3>';
-        oLi.innerHTML += '<p class="pro_price">HKD$' + data["productPrice"] + '</p>';
-        oLi.innerHTML += '<p class="pro_rank">' + data["productComment"] + 'K <i class="fa-regular fa-thumbs-up"></i></p>';
-        oLi.innerHTML += '<div class="add_btn">加入購物車</div>';
-        oUl.appendChild(oLi);
-    } */
 
     var aBtn = getClass(oBox, "add_btn");//獲取box下的所有添加購物車按鈕
     var number = 0;//初始化商品數量
@@ -68,6 +52,7 @@ window.onload = function() {
                 }
                 getAmount();
             }
+
             var delBtn = oDiv.lastChild.getElementsByTagName("a")[0];
             delBtn.onclick = function() {
                 var result = confirm("確定刪除嗎?");
@@ -76,8 +61,9 @@ window.onload = function() {
                     number--;
                     getAmount();
                 }
-            /* localStorage.removeItem('myCat'); */
+            // localStorage.removeItem('myCat');
             }
+
             var i_btn = document.getElementsByClassName("count_i");
             for (var k = 0; k < i_btn.length; k++) {
                 i_btn[k].onclick = function() {
@@ -102,6 +88,7 @@ window.onload = function() {
                     getAmount();
                 }
             }
+            
             //獲取所有數量減號按鈕
             var d_btn = document.getElementsByClassName("count_d");
             for (k = 0; k < i_btn.length; k++) {
